4e1ce64718bee8f1ef0c3c64a93910e584fa92ae
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-18 10:33:06 -0600

    commit tree 0e07a340cfa6e0638d1c927685a576941e3c3cd3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+  if(x > 42) {
+    return x - 42} 
+    else{ return 42 - x}
+    
+    //returns the number of blocks given a value
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264
+    } else {return (start - destination) * 264
+  }}
+
+  
+  function calculatesFarePrice(start, destination) {
+    const calculate = distanceTravelledInFeet(start, destination);
+        if(calculate <= 400){ return 0}
+        else if (calculate > 400 && calculate <= 2000) {
+          return 2.56
+        }
+        else if (calculate > 2000 && calculate < 2500) {
+          return 25
+        }else return 'cannot travel that far' 
+  }
+  
+  
